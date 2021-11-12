b1c1c66d1f07563f03fbcbd2e4527f7b0d639d11
unknown <mohammedismailpasha4044@gmail.com>
2021-11-12 08:47:50 +0530

    maven project

---
+++ src/main/java/in/ashokit/beans/EncodeDecodeUtils.java
@@ -0,0 +1,24 @@
+package in.ashokit.beans;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public  class EncodeDecodeUtils
+{
+	
+       public String encode(String text)
+	{
+			Encoder encoder = Base64.getEncoder();
+			String encodeToString = encoder.encodeToString(text.getBytes());
+			return encodeToString;
+			
+	}
+       public String decode(String encodedStr)
+       {
+    	   Decoder decoder = Base64.getDecoder();
+    	   byte[] decode = decoder.decode(encodedStr);
+    	   return new String(decode);
+       }
+	
+}
